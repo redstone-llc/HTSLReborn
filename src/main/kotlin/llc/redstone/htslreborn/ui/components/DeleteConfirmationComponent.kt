d4870c7604fe4b002ccce02c3d7034953ce6d045
Wekend <wekendmc@gmail.com>
2026-02-13 11:36:51 -0800

    style: modify file deletion dialogue

--- src/main/kotlin/llc/redstone/htslreborn/ui/components/DeleteConfirmationComponent.kt
+++ src/main/kotlin/llc/redstone/htslreborn/ui/components/DeleteConfirmationComponent.kt
@@ -10,18 +10,10 @@ import io.wispforest.owo.ui.core.Insets
 import io.wispforest.owo.ui.core.Sizing
 import io.wispforest.owo.ui.core.Surface
 import llc.redstone.htslreborn.HTSLReborn.CONFIG
-import llc.redstone.htslreborn.HTSLReborn.importingFile
-import llc.redstone.htslreborn.htslio.HTSLImporter
 import llc.redstone.htslreborn.ui.FileExplorer
 import llc.redstone.htslreborn.ui.FileHandler
-import llc.redstone.systemsapi.importer.ActionContainer
-import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.text.Text
-import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.deleteExisting
-import kotlin.io.path.deleteRecursively
-import kotlin.io.path.isDirectory
-import kotlin.io.path.name
+import kotlin.io.path.*
 
 class DeleteConfirmationComponent(
     horizontalSizing: Sizing, verticalSizing: Sizing,
@@ -36,28 +28,28 @@ class DeleteConfirmationComponent(
             children(
                 listOf(
                     UIComponents.label(Text.translatable("htslreborn.explorer.confirm.delete.title")),
-                    UIComponents.label(Text.translatable("htslreborn.explorer.confirm.delete.message")),
-                    UIComponents.label(Text.of(fileName() + "?")),
+                    UIComponents.spacer(2),
+                    UIComponents.label(Text.translatable("htslreborn.explorer.confirm.delete.message").withColor(0x7E7E7E)),
+                    UIComponents.label(Text.literal(fileName() + "?").withColor(0x7E7E7E)),
                     UIComponents.spacer(2),
                     UIContainers.horizontalFlow(Sizing.fill(), Sizing.content()).apply {
                         horizontalAlignment(HorizontalAlignment.CENTER)
+                        gap(4)
                         children(
                             listOf(
-                                UIComponents.button(Text.translatable("htslreborn.explorer.confirm.delete.button.confirm")) {
-                                    click(it)
-                                }.apply {
-                                    id("confirm")
-                                    horizontalSizing(Sizing.fill(50))
-                                    horizontalAlignment(HorizontalAlignment.CENTER)
-                                    renderer(ButtonComponent.Renderer.flat(0x00000000, 0x50000000, 0x00000000))
-                                },
                                 UIComponents.button(Text.translatable("htslreborn.explorer.confirm.delete.button.cancel")) {
                                     click(it)
                                 }.apply {
                                     id("cancel")
-                                    horizontalSizing(Sizing.fill(50))
+                                    horizontalSizing(Sizing.expand(40))
                                     horizontalAlignment(HorizontalAlignment.CENTER)
-                                    renderer(ButtonComponent.Renderer.flat(0x00000000, 0x50000000, 0x00000000))
+                                },
+                                UIComponents.button(Text.translatable("htslreborn.explorer.confirm.delete.button.confirm")) {
+                                    click(it)
+                                }.apply {
+                                    id("confirm")
+                                    horizontalSizing(Sizing.expand(40))
+                                    horizontalAlignment(HorizontalAlignment.CENTER)
                                 }
                             )
                         )
